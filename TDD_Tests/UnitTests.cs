764626e7b56415b0eb6e8690d74ae141b19b51bc
kajos <kajos@KAJOS>
2025-04-24 02:01:03 +0700

    + Добавлен тест Player_1_Win_Vertical_Line()

--- TDD_Tests/UnitTests.cs
+++ TDD_Tests/UnitTests.cs
@@ -81,5 +81,30 @@ namespace TDD_Tests
 
             Assert.IsTrue(result);
         }
+
+        [Test]
+        public void Player_1_Win_Vertical_Line()
+        {
+            Game game = new Game();
+
+            Button[,] buttons = new Button[3, 3]
+            {
+                { new Button(), new Button(), new Button() },
+                { new Button(), new Button(), new Button() },
+                { new Button(), new Button(), new Button() }
+            };
+
+            var turn_player_label = new Label();
+
+            game.Next_Turn(buttons[0, 0], turn_player_label);   // Игрок 1
+            game.Next_Turn(buttons[0, 1], turn_player_label);   // Игрок 2
+            game.Next_Turn(buttons[1, 0], turn_player_label);   // Игрок 1
+            game.Next_Turn(buttons[1, 1], turn_player_label);   // Игрок 2
+            game.Next_Turn(buttons[2, 0], turn_player_label);   // Игрок 1 - победил
+
+            bool result = game.Check_Win(buttons);
+
+            Assert.IsTrue(result);
+        }
     }
 }
