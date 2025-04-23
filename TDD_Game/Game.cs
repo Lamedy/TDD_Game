0199c6f09bc5d4632d52ab1f28723a7b39cfa2cc
kajos <kajos@KAJOS>
2025-04-24 01:28:12 +0700

    + Добавлен метод Start_Game

--- TDD_Game/Game.cs
+++ TDD_Game/Game.cs
@@ -8,5 +8,20 @@ namespace TDD_Game
 {
     public class Game
     {
+        public bool Start_Game(
+            Button startGameButton,
+            Button[,] buttons,
+            Label turn_player)
+        {
+            startGameButton.Visible = false;
+            foreach (Button button in buttons)
+            {
+                button.Visible = true;
+            }
+            turn_player.Visible = true;
+            turn_player.Text = "Turn player 1";
+
+            return true;
+        }
     }
 }
