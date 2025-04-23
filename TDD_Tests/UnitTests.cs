4063424e6aa367f4c1ed5a5ae9a6447b428960ed
kajos <kajos@KAJOS>
2025-04-24 01:25:56 +0700

    + Добавлен тест Start_Game()

--- TDD_Tests/UnitTests.cs
+++ TDD_Tests/UnitTests.cs
@@ -1,3 +1,4 @@
+using System.Windows.Forms;
 using TDD_Game;
 
 namespace TDD_Tests
@@ -10,6 +11,37 @@ namespace TDD_Tests
             Game game = new Game();
 
             Assert.NotNull(game, "Game is not exist.");
+        }
+
+        [Test]
+        public void Start_Game()
+        {
+            Game game = new Game();
+
+            var startGameButton = new Button();
+
+            Button[,] buttons = new Button[3, 3]
+            {
+                { new Button(), new Button(), new Button() },
+                { new Button(), new Button(), new Button() },
+                { new Button(), new Button(), new Button() }
+            };
+
+            var turn_player = new Label();
+
+            bool result = game.Start_Game(
+                startGameButton,
+                buttons,
+                turn_player
+            );
+
+            Assert.IsFalse(startGameButton.Visible);
+            foreach (var button in buttons)
+            {
+                Assert.IsTrue(button.Visible);
+            }
+            Assert.IsTrue(turn_player.Visible);
+            Assert.That(turn_player.Text, Is.EqualTo("Turn player 1"));
         }
     }
 }
